5daf42884ff48f3f0fc4bebd8e7c066e6062c56d
Shubham Vishwakarma <shubhamvis98@fossfrog.in>
2025-10-07 15:41:20 +0530

    add support for indio-225ax-v2
    Signed-off-by: Shubham Vishwakarma <shubhamvis98@fossfrog.in>

--- feeds/mediatek-sdk/mediatek/mt7981/base-files/lib/upgrade/platform.sh
+++ feeds/mediatek-sdk/mediatek/mt7981/base-files/lib/upgrade/platform.sh
@@ -58,6 +58,10 @@ platform_do_upgrade() {
 		fi
 		nand_do_upgrade "$1"
 		;;
+	indio,um-225ax-v2)
+		CI_UBIPART="ubi"
+		nand_do_upgrade "$1"
+		;;
 	senao,iap2300m|\
 	emplus,wap588m|\
 	senao,jeap6500)
@@ -80,6 +84,7 @@ platform_check_image() {
 	case "$board" in
 	edgecore,eap111|\
 	edgecore,eap112|\
+	indio,um-225ax-v2|\
 	senao,iap2300m|\
 	emplus,wap588m|\
 	senao,jeap6500)
